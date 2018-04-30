84ecd5e701d855b61ee0744ac82acde3d5617dea
jaymccormick <jaymccormi@gmail.com>
2018-04-30 10:15:31 -0400

    dom content event, call back

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-// li collection for laws
-const laws = document.getElementsByTagName('li');
-// get input
-const indexText = document.getElementById('whichBold');
-// get button
-const button = document.getElementById('bolden');
+// dom content loaded listener, callback function
+document.addEventListener('DOMContentLoaded', () => {
 
-// button listener
-button.addEventListener('click', (e) => {
-  // get input as int
-  const index = parseInt(indexText.value, 10);
+  // li collection for laws
+  const laws = document.getElementsByTagName('li');
+  // get input
+  const indexText = document.getElementById('whichBold');
+  // get button
+  const button = document.getElementById('bolden');
 
-  // loop through law in laws collection and change fontWeight
-  for (let i = 0; i < laws.length; i++){
-    let law = laws[i];
-    if (index === i){
-      law.style.fontWeight = 'bold';
-    } else {
-      law.style.fontWeight = '';
+  // button listener
+  button.addEventListener('click', (e) => {
+    // get input as int
+    const index = parseInt(indexText.value, 10);
+
+    // loop through law in laws collection and change fontWeight
+    for (let i = 0; i < laws.length; i++){
+      let law = laws[i];
+      if (index === i){
+        law.style.fontWeight = 'bold';
+      } else {
+        law.style.fontWeight = '';
+      }
     }
-  }
-  // reset input field
-  indexText.value = '';
+    // reset input field
+    indexText.value = '';
+  });
 });
