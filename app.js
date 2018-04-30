fd4e323b3c958a1940a8ba298eb3a4e57c390a53
jaymccormick <jaymccormi@gmail.com>
2018-04-30 09:45:44 -0400

    globals

---
+++ app.js
@@ -0,0 +1,6 @@
+// li collection for laws
+const laws = document.getElementsByTagName('li');
+// get input
+const indexText = document.getElementById('whichBold');
+// get button
+const button = document.getElementById('bolden');
