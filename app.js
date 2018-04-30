88c0bdf9f6b4beabcfefd2492e405646900c4f75
jaymccormick <jaymccormi@gmail.com>
2018-04-30 09:46:59 -0400

    button boldener handler

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const laws = document.getElementsByTagName('li');
 const indexText = document.getElementById('whichBold');
 // get button
 const button = document.getElementById('bolden');
+
+// button listener
+button.addEventListener('click', (e) => {
+  // get input as int
+  const index = parseInt(indexText.value, 10);
+
+  // loop through law in laws collection and change fontWeight
+  for (let i = 0; i < laws.length; i++){
+    let law = laws[i];
+    if (index === i){
+      law.style.fontWeight = 'bold';
+    } else {
+      law.style.fontWeight = '';
+    }
+  }
+  // reset input field
+  indexText.value = '';
+});
